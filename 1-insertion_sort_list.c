b247b945d66e39c91307c49b1a4df94eee043669
Joshua Ibitoye <joshuaibitoye111@gmail.com>
2022-10-10 23:12:21 +0000

    insert

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,50 @@
+#include "sort.h"
+void swap_nodes(listint_t *node_1, listint_t *node_2, listint_t **head)
+{
+    if (node_1->prev == NULL)
+    {
+        node_1->next = node_2->next;
+        node_2->next->prev = node_1;
+        node_2->prev = node_1->prev;
+        node_2->next = node_1;
+        node_1->prev = node_2; 
+        *head = node_2;
+    }
+    else
+    {
+        node_1->next = node_2->next;
+        if (node_2->next != NULL)
+           node_2->next->prev = node_1;
+        node_1->prev->next = node_2;
+        node_2->prev = node_1->prev;
+        node_2->next = node_1;
+        node_1->prev = node_2;
+    }
+    
+}
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *node = *list;
+    listint_t *runner = malloc(sizeof(listint_t));
+    while (node->next != NULL)
+    {
+        if(node->n > node->next->n)
+        {
+            swap_nodes(node,node->next, list);
+            print_list(*list);
+            runner = node->prev;
+            node = node->prev;
+            while (runner->prev != NULL)
+            {
+                if(runner->n < runner->prev->n)
+                {
+                    swap_nodes(runner->prev, runner, list);
+                    print_list(*list);
+                    runner = runner->next;
+                }
+                runner = runner->prev;
+            }
+        }
+        node = node->next;
+    }
+}
